be954cc486537a4e2512d723cdef779bdb2e3de3
David R Connell <davidconnell12@gmail.com>
2019-07-26 18:26:33 -0500

    Switch to pulseaudio

--- volume.lisp
+++ volume.lisp
@@ -1,8 +1,6 @@
-;;; Setup pavol
-
 ;;; Load package
 (in-package :stumpwm)
 
-(define-key *top-map* (kbd "XF86AudioLowerVolume") "amixer-Front-1-")
-(define-key *top-map* (kbd "XF86AudioRaiseVolume") "amixer-Front-1+")
-(define-key *top-map* (kbd "XF86AudioMute") "amixer-Master-toggle pulse")
+(define-key *top-map* (kbd "XF86AudioLowerVolume") "exec pulseaudio-ctl down")
+(define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec pulseaudio-ctl up")
+(define-key *top-map* (kbd "XF86AudioMute") "exec pulseaudio-ctl mute")
