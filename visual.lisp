64c3fb2dab2d64a0d48aec2d95c97e811426fa51
David R Connell <davidconnell12@gmail.com>
2019-07-27 21:49:12 -0500

    Add battery and volume info to mode-line

--- visual.lisp
+++ visual.lisp
@@ -18,14 +18,38 @@
 	(car (last *colors*)) fg)
   (update-color-map (current-screen)))
 
-(load-module :battery-portable)
 (set-font "-*-dejavu sans mono-*-r-*-*-16-*-*-*-*-*-*-*")
 
+;; Mode line
+(defun get-battery-info ()
+  (let* ((battery-stats (run-shell-command-list
+                         "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk -F\": *\" '/(percentage|state):/ { print $2 }'")
+                         )
+         (state (first battery-stats))
+         (percent (second battery-stats)))
+    (if (not (emptyp percent))
+        (if (not (string= "discharging" state))
+         (concat "~" percent " | ")
+         (concat percent " | "))
+      (concat ""))))
+
+(defun get-volume ()
+  (let* ((volume-stats (split-string
+                        (run-shell-command "pulseaudio-ctl full-status" t)
+                        " "))
+         (percent (first volume-stats))
+         (mutesign (if (string= (second volume-stats) "yes")
+                    "x"
+                    "")))
+    (concat mutesign percent " | ")))
+
 (setf *screen-mode-line-format*
-      (list " " '(:eval (time-format "%H:%M")) " | %W"))
+      (list " " '(:eval (time-format "%H:%M")) " | "
+            '(:eval (get-battery-info))
+            '(:eval (get-volume))
+            "%W"))
 
 (setf *window-format* "%n %10c |")
 
-;; Turn on the modeline
 (loop for head in (screen-heads (current-screen)) do
       (toggle-mode-line (current-screen) head))
