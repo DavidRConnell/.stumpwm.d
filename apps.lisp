552f0714927a056dac2c10f59886a8db954065f1
David R Connell <davidconnell12@gmail.com>
2019-07-31 23:48:39 -0500

    Change program-bindings so they don't move windows
    Previously windows moved to new frame if called from a frame different
    than there current frame. Now this behavior is left to the capital
    variant of the function akin to how the capitialized j,k,l,h commands
    move windows.

--- apps.lisp
+++ apps.lisp
@@ -1,11 +1,11 @@
 (in-package :stumpwm)
 
 (defmacro make-program-binding (program-name window-class key &optional alias)
-  "Add keybindings for a program to *root-map*.
-window-class is the windows-class
-key is the key for the keybinding.
-C-key will open a new instance of program-name,
-key will run-or-pull program-name."
+  "Add keybindings for a program to *root-map*. window-class is the
+windows-class key is the key for the keybinding. C-key will open a new instance
+of program-name, key will run-or-raise program-name, and (string-upcase key)
+will run-or-pull program-name. Sticking to the theme of capitialized commands
+move windows."
 
   (if (not alias)
       (setf alias program-name))
@@ -14,10 +14,16 @@ key will run-or-pull program-name."
      (defcommand ,(intern (format nil "run-new-~a" alias)) () ()
        (run-shell-command ,program-name))
 
+     (defcommand ,(intern (format nil "run-or-raise-~a" alias)) () ()
+       (run-or-raise ,program-name '(:class ,window-class)))
+
      (defcommand ,(intern (format nil "run-or-pull-~a" alias)) () ()
        (run-or-pull ,program-name '(:class ,window-class)))
 
      (define-key *root-map* (kbd ,(format nil "~a" key))
+       ,(format nil "run-or-raise-~a" alias))
+
+     (define-key *root-map* (kbd ,(format nil "~a" (string-upcase key)))
        ,(format nil "run-or-pull-~a" alias))
 
      (define-key *root-map* (kbd ,(format nil "C-~a" key))
