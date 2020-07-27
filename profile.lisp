478a89dd2183b4f653303e05605341610f57db2e
David R Connell <davidconnell12@gmail.com>
2020-07-26 22:31:56 -0500

    Change: return to qutebrowser

--- profile.lisp
+++ profile.lisp
@@ -1,6 +1,6 @@
 (in-package :stumpwm)
 
-(defvar *browser* "next")
+(defvar *browser* "qutebrowser")
 (defvar *alt-browser* "chromium")
 (defvar *terminal* "alacritty")
 (defvar *editor* "emacsclient -c")
